fc2a35af8ff0042e61d7a0cba2263ca58b5ed99d
Stefan Kupresak <stefan_vg@hotmail.com>
2023-04-07 16:15:01 +0200

    complete main page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import partytown from "@astrojs/partytown";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), partytown()]
+  site: "https://example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    partytown(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+  ],
 });
