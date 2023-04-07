a8bd387c99a35d2c1eada80b18d507738fb288be
Stefan Kupresak <stefan_vg@hotmail.com>
2023-04-07 18:03:14 +0200

    add parts with highlight

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  safelist: [{ pattern: /hljs+/ }],
   theme: {
+    hljs: {
+      theme: "night-owl",
+    },
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("tailwind-highlightjs"),
+  ],
 };
